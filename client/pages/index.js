a51bb8a7f27b16a454d0ef40d4f4f876d19edce6
Ahmed <ahmed@hadjou.com>
2021-02-04 14:04:41 +0000

    init next and home page

---
+++ client/pages/index.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:1337/articles")
+      .then((res) => setPosts(res.data));
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Posts</h1>
+        {posts?.map((p) => (
+          <div key={p.id}>
+            <p>{p.title}</p>
+            <p>{p.body}</p>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}

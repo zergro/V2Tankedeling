ed959a3e7d3ae90e1537fb615e3bd16841bba4b5
zergro <erling_95@hotmail.com>
2021-02-10 12:56:41 +0100

    Added my project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0787e38c5f5d21e630727d01c40086bc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9cdc2ee2-3f22-4512-bd50-a120586e5ab0'
+};
